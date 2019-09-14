9766b4af71724db23c562319ffa5a65ca79c2ee1
jmjablons <juda.jablonska@gmail.com>
2019-09-14 19:25:59 +0200

    participant control done

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -1,10 +1,13 @@
 package com.company.enroller.controllers;
 
+import java.net.URISyntaxException;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,7 @@ public class ParticipantRestController {
 		Collection<Participant> participants = participantService.getAll();
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
 	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
 	     Participant participant = participantService.findByLogin(login);
@@ -33,34 +37,37 @@ public class ParticipantRestController {
 	     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
 	 }
 	
-	 @RequestMapping(value = "", method = RequestMethod.POST)
-	 public ResponseEntity<?> registerParticipant(@RequestBody Participant participant){
-		 Participant foundParticipant = participantService.findByLogin(participant.getLogin());
-		 if (foundParticipant != null) {
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	 public ResponseEntity<?> addParticipant(@RequestBody Participant participant) throws URISyntaxException {
+		 //Participant foundParticipants = participantService.findByLogin(participant.getLogin());
+		 if(participantService.findByLogin(participant.getLogin()) != null) {
 			 return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+		 } 
+			 participantService.add(participant);
+			 return new ResponseEntity<Participant>(participant, HttpStatus.CREATED);
 		 }
-		 participantService.add(participant);
-		 return new ResponseEntity<Participant>(participant, HttpStatus.CREATED);
-	 }
 	 
-	 @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-		public ResponseEntity<?> deleteParticipant(@PathVariable("id") String login) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+		public ResponseEntity<?> deleteParticipant(@PathVariable("id") String login) throws URISyntaxException {
 		     Participant participant = participantService.findByLogin(login);
 		     if (participant == null) {
 		         return new ResponseEntity(HttpStatus.NOT_FOUND);
 		     }
 		     participantService.delete(participant);
-		     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+		     return new ResponseEntity(HttpStatus.GONE);
+		     //return new ResponseEntity<Participant>(participant, HttpStatus.GONE);
 		 }
 	 
-	 @RequestMapping(value = "/{id}", method = RequestMethod.POST)
-		public ResponseEntity<?> updateParticipant(@PathVariable("id") String login) {
-		     Participant participant = participantService.findByLogin(login);
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+		public ResponseEntity<?> updateParticipant(@PathVariable("id") String login,
+				@RequestBody Participant dParticipant) throws URISyntaxException {
+		 Participant participant = participantService.findByLogin(login);
 		     if (participant == null) {
 		         return new ResponseEntity(HttpStatus.NOT_FOUND);
 		     }
+		     participant.setPassword(dParticipant.getPassword());
 		     participantService.update(participant);
-		     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+		     return new ResponseEntity<Participant>(HttpStatus.OK);
 		 }
 
 }
